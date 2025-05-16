da22fb0428ca5c9deaff8e96c4f9bd8fb50e5269
Rajkumar Nerall <rajkumarnerall@Rajkumars-MBP.attlocal.net>
2025-05-15 23:19:12 -0500

    week2-lab-assignment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require('path'); 
+const auth = require('http-auth');
+const mongoose = require('mongoose');
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 
+const basic = auth.basic({ file: path.join(__dirname, '../users.htpasswd'), });
 
 router.get(
     "/",
@@ -22,7 +27,14 @@ router.post(
     console.log(req.body);
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-      res.send("Thank you for your registration!");
+      const registration = new Registration(req.body);
+      registration.save()
+      .then(()=> {
+        res.send("Thank you for your registration!");
+      }).catch((err)=> {
+        console.log(err)
+        res.send("Sorry! Something went wrong.");
+      });
     } else {
       res.render("form", {
         title: "Registration form",
@@ -32,5 +44,18 @@ router.post(
     }
   }
 );
+
+router.get('/registrations', basic.check((req, res) => {
+  Registration.find()
+  .then((registrations)=>{
+    res.render("index", {
+      title: "Listing registrations",
+      registrations
+    });
+  }).catch((err)=> {
+    console.log(err)
+    res.send("Sorry! Something went wrong.");
+  });
+}));
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,3 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('open', ()=> {
+    console.log('Mongoose connection open')
+}).on('error', (error)=>{
+    console.log(`Connection error: ${error.message}`)
+})
+
+require('./models/Registration')
 const app = require('./app');
 
 const server = app.listen(3000, function(){
